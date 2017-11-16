02698dc5515e5828a49e16ebec50080a40c2045a
shanmahesh <shan.mahesh@gmail.com>
2017-11-16 17:07:07 -0500

    releasing models

---
+++ springdemo/src/main/java/com/spring5/springdemo/model/Author.java
@@ -0,0 +1,50 @@
+package com.spring5.springdemo.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String fistName;
+	private String lastname;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<>();
+	
+	
+	public Set<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFistName() {
+		return fistName;
+	}
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+}
