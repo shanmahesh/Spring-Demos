4ab58bb4602e0e76bdca71426a3c88e74d9f097a
shanmahesh <shan.mahesh@gmail.com>
2017-11-30 16:26:14 -0500

    recipie project

---
+++ recipie/src/main/java/com/recipie/RecipieApplication.java
@@ -0,0 +1,12 @@
+package com.recipie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipieApplication.class, args);
+	}
+}
